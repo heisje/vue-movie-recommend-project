583644a4a4e7183d10ff3de56d49b2051704eca1
bbookng <bbookng@gmail.com>
2022-11-11 10:33:49 +0900

    bootstrap

--- my-vue-pjt/src/views/RandomView.vue
+++ my-vue-pjt/src/views/RandomView.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
         <button @click="getRandomMovie">PICK ME PICK ME PICK ME UP</button>
-        <h1>RandomView</h1>
+        <div class="d-grid gap-4 col-4 mx-auto">
+            <div v-if="movie" class="card" style="width: 100%;">
+                <img :src="`https://image.tmdb.org/t/p/original/${ movie.poster_path }`" class="card-img-top" alt="PICK A MOVIE">
+            <div class="card-body">
+                <h4 class="card-title fw-bold">{{ movie.title }}</h4>
+            </div>
+      </div>          
+    </div>
     </div>
 </template>
 
 <script>
 import _ from 'lodash'
+
 export default {
     name:'RandomView',
     data(){
@@ -16,16 +24,11 @@ export default {
     },
     methods: {
         getRandomMovie() {
-        console.log(this.$store.state.topRanked)
         this.movie = _.sample(this.$store.state.topRanked)
-        console.log(this.movie)
-        console.log(1)
-
         }
     }
 }
 </script>
 
 <style>
-
 </style>
