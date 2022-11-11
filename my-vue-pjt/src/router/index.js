7ea0114a90ffa9f1916b6b8a0e62829ce11dafd3
bbookng <bbookng@gmail.com>
2022-11-11 09:30:45 +0900

    router-settings

--- my-vue-pjt/src/router/index.js
+++ my-vue-pjt/src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MovieView from '@/views/MovieView.vue'
+import RandomView from '@/views/RandomView.vue'
+import WatchListView from '@/views/WatchListView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/movies',
+    name: 'movie',
+    component: MovieView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/random',
+    name: 'random',
+    component: RandomView
+  },
+  {
+    path: '/watch-list',
+    name: 'watchlist',
+    component: WatchListView
   }
+  
 ]
 
 const router = new VueRouter({
