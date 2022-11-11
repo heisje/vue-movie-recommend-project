0371c91b8a90130229037467d64fc0253b0a16cd
heisje <heeje1996@gmail.com>
2022-11-11 09:34:17 +0900

    기본구조 채워넣음

---
+++ my-vue-pjt/src/views/MovieView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>MovieView</h1>
+        <MovieCard/>
+    </div>
+</template>
+
+<script>
+import MovieCard from '@/components/MovieCard.vue';
+export default {
+    name:'MovieView',
+    components:{
+        MovieCard,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ my-vue-pjt/src/views/WatchListView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>WatchListView</h1>
+        <WatchListForm/>
+        <WatchListItem/>
+    </div>
+</template>
+
+<script>
+import WatchListForm from '@/components/WatchListForm.vue';
+import WatchListItem from '@/components/WatchListItem.vue';
+export default {
+    name:'WatchListView',
+    components:{
+        WatchListForm,
+        WatchListItem,
+    }
+}
+</script>
+
+<style>
+
+</style>
